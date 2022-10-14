5f0e139c5c7a97a551cb1059b1055fbaab7cc4dd
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-10-14 14:58:09 +0000

    [Add]user機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :new
+    end
   end
 
   def index
+    @book = Book.all
+    @user = User.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+
 end
